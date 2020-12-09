ee56a6a16ab577b7be8b35ae3022d6b764510361
Rajesh Kumar Singh <rajeshemac@gmail.com>
2020-12-09 20:45:03 +0530

    Added ViewSets to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.viewsets import ViewSet
 
 from profiles_api import serializers
 
@@ -46,3 +47,53 @@ class HelloAPIView(APIView):
         """Handle a partial update of an object"""
 
         return Response({'Method': 'DELETE'})
+
+class HelloViewSet(ViewSet):
+    """Test API Viewset"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a Hello Message"""
+
+        a_viewset = [
+        'Uses actions (list, create, retrieve, update, partial_update)',
+        'Automatically maps URLs using routers',
+        'Provides more functionality with less code'
+        ]
+
+        return Response({'message': 'Hello','a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new Hello Message"""
+
+        serializer = self.serializer_class(data.request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Retrive an object by it's ID"""
+
+        return Response({'HTTP Method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Updates an object by it's ID"""
+
+        return Response({'HTTP Method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Partial updates an object by it's ID"""
+
+        return Response({'HTTP Method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Remove an object by it's ID"""
+
+        return Response({'HTTP Method': 'DELETE'})
